9d1f3512c9e61980480b5285bee2e84a9052e188
BoNGoNAWT <58984645+BoNGoNAWT@users.noreply.github.com>
2020-06-10 17:10:47 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,88 +1,186 @@
 import discord
+from discord.ext import commands
+import random
+from discord.utils import get
+import shutil
+import json
+import asyncio
 import os
+from itertools import cycle
+import colorsys
+from discord import Game, Embed, Color, Status, ChannelType
+import datetime
+import sqlite3
+import math
+import sys, traceback
+import time
+from datetime import timedelta
+from collections import OrderedDict, deque, Counter
 
-client = discord.Client()
 
-@client.event
+Bot = commands.Bot(command_prefix = ".")
+
+@Bot.event
 async def on_ready():
     print("Даров, бать")
 
-@client.event
-async def on_raw_reaction_add(payload):
-    message_id = payload.message_id
-    if message_id == 711260552592359555:
-        guild_id = payload.guild_id
-        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+@Bot.command(pass_context= True)
+@commands.has_permissions(manage_messages =True)
+async def clear(ctx, amount = 100):
+    await ctx.channel.purge(limit = amount)
+ 
 
-        if payload.emoji.name == 'r6':
-            role = discord.utils.get(guild.roles, name='r6')
-        elif payload.emoji.name == 'minecraft':
-            role = discord.utils.get(guild.roles, name='minecraft')
-        elif payload.emoji.name == 'farcry':
-            role = discord.utils.get(guild.roles, name='farcry')
-        elif payload.emoji.name == 'gmod':
-            role = discord.utils.get(guild.roles, name='gmod')
-        elif payload.emoji.name == 'payday2':
-            role = discord.utils.get(guild.roles, name='payday2')
-        elif payload.emoji.name == 'apex':
-            role = discord.utils.get(guild.roles, name='apex')
-        elif payload.emoji.name == 'gta5':
-            role = discord.utils.get(guild.roles, name='gta5')
-        elif payload.emoji.name == 'pubg':
-            role = discord.utils.get(guild.roles, name='pubg')
-        elif payload.emoji.name == 'csgo':
-            role = discord.utils.get(guild.roles, name='csgo')
-        else:
-            role = discord.utils.get(guild.roles, name = payload.emoji.name)
 
-        if role is not None:
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            if member is not None:
-                await member.add_roles(role)
-                print('Done')
-            else:
-                print('Member not found')
-        else:
-            print('Role not found')
+@Bot.command(pass_context = True)
+@commands.has_permissions(administrator = True)
 
-@client.event
-async def on_raw_reaction_remove(payload):
-    message_id = payload.message_id
-    if message_id == 711260552592359555:
-        guild_id = payload.guild_id
-        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+async def kick(ctx, member: discord.Member, *, reason = None):
+    await ctx.channel.purge(limit = 1)
+    emb = discord.Embed(title = 'Kick', colour = discord.Color.purple())
 
-        if payload.emoji.name == 'r6':
-            role = discord.utils.get(guild.roles, name='r6')
-        elif payload.emoji.name == 'minecraft':
-            role = discord.utils.get(guild.roles, name='minecraft')
-        elif payload.emoji.name == 'farcry':
-            role = discord.utils.get(guild.roles, name='farcry')
-        elif payload.emoji.name == 'gmod':
-            role = discord.utils.get(guild.roles, name='gmod')
-        elif payload.emoji.name == 'payday2':
-            role = discord.utils.get(guild.roles, name='payday2')
-        elif payload.emoji.name == 'apex':
-            role = discord.utils.get(guild.roles, name='apex')
-        elif payload.emoji.name == 'gta5':
-            role = discord.utils.get(guild.roles, name='gta5')
-        elif payload.emoji.name == 'pubg':
-            role = discord.utils.get(guild.roles, name='pubg')
-        elif payload.emoji.name == 'csgo':
-            role = discord.utils.get(guild.roles, name='csgo')
-        else:
-            role = discord.utils.get(guild.roles, name = payload.emoji.name)
+    await member.kick(reason = reason)
+    emb.set_author(name = member.name, icon_url = member.avatar_url)
+    emb.add_field(name = 'Kick', value = 'Пинком был изгнан {}'.format(member.mention))
+    await ctx.send(embed = emb)
+    await member.send(embed = discord.Embed(title = 'Вас выгнали с сервера Dark Side!', colour = discord.Color.purple()))
 
-        if role is not None:
-            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
-            if member is not None:
-                await member.remove_roles(role)
-                print('Done')
-            else:
-                print('Member not found')
-        else:
-            print('Role not found')
+@Bot.command(pass_context = True)
+@commands.has_permissions(administrator = True)
+
+async def ban(ctx, member: discord.Member, *, reason = None):
+    await ctx.channel.purge(limit = 1)
+    emb = discord.Embed(title = 'Ban', colour = discord.Color.red())
+    
+
+    await member.ban(reason = reason)
+
+    emb.set_author(name = member.name, icon_url = member.avatar_url)
+    emb.add_field(name = 'Banned', value = 'Великая печать изгнания возложена на {}'.format(member.mention))
+    emb.set_footer(text = 'Был забанен администратором {}'.format(ctx.author.name), icon_url = ctx.author_url)
+    await ctx.send(embed = emb)
+    await member.send(embed = discord.Embed(title = 'Вы были забанены администратором{}'.format(ctx.author.name), icon_url = ctx.author_url, colour = discord.Color.red()))
+
+@Bot.command(pass_context= True)
+@commands.has_permissions(administrator = True)
+
+async def unban(ctx, *, member):
+    await ctx.channel.purge(limit = 1)
+
+    banned_users = await ctx.guild.bans()
+
+
+    for ban_entry in banned_users:
+        user = ban_entry.user
+        emb = discord.Embed(title = 'Unban', colour = discord.Color.green())
+
+
+        await ctx.guild.unban(user)
+        emb.set_author(name = member.name)
+        
+        await ctx.send(f'Великая печать бана снята с {user.menttion}')
+
+        return
+
+@Bot.command(pass_context= True)
+
+
+async def help(ctx):
+    emb = discord.Embed(title = 'Команды:')
+
+    emb.add_field(name = 'clear', value = 'Очичтка чата')
+    emb.add_field(name = 'kick', value = 'Выгнать участника с сервера')
+    emb.add_field(name = 'ban', value = 'Забанить участника')
+    emb.add_field(name = 'unban', value = 'Розбанить участника')
+    emb.add_field(name = 'say', value = 'Отправить сообщение от имени бота')
+    emb.add_field(name = 'send', value = 'Отправить личное сообщение от имени бота')
+    emb.add_field(name = 'fck', value = 'Послать к трём чертям')
+    emb.add_field(name = 'hug', value = 'Обнять')
+    emb.add_field(name = 'kiss', value = 'Поцеловать')
+    emb.add_field(name = 'kill', value = 'Убить')
+
+    await ctx.send(embed = emb)
+
+@Bot.command()
+@commands.has_permissions(manage_messages =True)
+async def say(ctx, *, msg):
+    await ctx.message.delete()
+    await ctx.send("{}".format(msg))
+
+@Bot.command()
+async def fck(ctx, member: discord.Member):
+    await ctx.message.delete()
+    await ctx.send(f"{ctx.author.mention} послал(a) к трём чертям {member.mention}")
+
+
+@Bot.command()
+async def hug(ctx, member: discord.Member):
+    await ctx.message.delete()
+    await ctx.send(f"{ctx.author.mention} обнял(а) {member.mention}")
+
+@Bot.command()
+async def artic(ctx, member: discord.Member):
+    await ctx.message.delete()
+    await ctx.send(f"{ctx.author.mention} росчленил(а) {member.mention}")
+
+@Bot.command()
+async def kiss(ctx, member: discord.Member):
+    await ctx.message.delete()
+    await ctx.send(f"{ctx.author.mention} поцеловал(а) {member.mention}")
+
+@Bot.command()
+async def kill(ctx, member: discord.Member):
+    await ctx.message.delete()
+    await ctx.send(f"{ctx.author.mention} убил (а) {member.mention}")
+
+@Bot.command()
+async def burn(ctx, member: discord.Member):
+    await ctx.message.delete()
+    await ctx.send(f"{ctx.author.mention} кинул(а) {member.mention} в крематорий")
+
+@Bot.command()
+async def sex(ctx, member: discord.Member):
+    await ctx.message.delete()
+    await ctx.send(f"{ctx.author.mention} подверг(ла) к жесткому сексу {member.mention}")
+
+@Bot.command()
+async def suicide(ctx, member: discord.Member):
+    vvr = ['утонул(а) в луже', 'умер(ла) на унитазе', 'умер(ла) во время мастурбауии', 'утонул(ла) в луже', 'героически погиб(ла) сражаясь за печеньку', 'умер(ла), причина: кусь']
+    await ctx.message.delete()
+    await ctx.send(f"{ctx.author.mention}, {random.choice(vvr)}")
+ 
+
+@Bot.command()
+@commands.has_permissions( administrator = True)
+async def send(ctx, member: discord.Member, *, msg):
+    await ctx.message.delete()
+    await member.send('{}'.format(msg))
+    await ctx.send('Сообщение отправлено')
+    
+
+
+@Bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+@Bot.command()
+async def king(ctx):
+    await ctx.send("Kong!")
+
+@ban.error
+async def error_command(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send(embed = discord.Embed(title = '{} у вас недостаточно прав!'.format(ctx.author.name), colour = discord.Color.red()))
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(embed = discord.Embed(title = '{} укажите аргумент!'.format(ctx.author.name), colour = discord.Color.red()))
+
+
+@kick.error
+async def kick_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send(embed = discord.Embed(title = '{} у вас недостаточно прав!'.format(ctx.author.name), colour = discord.Color.red()))
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(embed = discord.Embed(title = '{} укажите аргумент!'.format(ctx.author.name), colour = discord.Color.red()))
 
 token = os.environ.get('BOT_TOKEN')
-client.run(str(token))
+Bot.run(str(token))
         
