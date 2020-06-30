2661640e10309c9e1562adeabbd8980dbd655198
BoNGoNAWT <58984645+BoNGoNAWT@users.noreply.github.com>
2020-06-30 14:22:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,16 +22,6 @@ Bot = commands.Bot(command_prefix = ".")
 
 Bot.remove_command('help')
 
-initial_extensions = ['cogs.voice']
-
-if __name__ == '__main__':
-    for extension in initial_extensions:
-        try:
-            Bot.load_extension(extension)
-        except Exception as e:
-            print(f'Failed to load extension {extension}.', file=sys.stderr)
-            traceback.print_exc()
-
 @Bot.event
 async def on_ready():
     print("Даров, бать")
